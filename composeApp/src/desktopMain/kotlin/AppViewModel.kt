3c799e27f3f0ee5bca4aa8aaf8023756e549bb63
Stefan Büche <stefan.bueche@gls-itservices.com>
2024-10-28 09:46:50 +0100

    Added logging to help with error analysis.

--- composeApp/src/desktopMain/kotlin/AppViewModel.kt
+++ composeApp/src/desktopMain/kotlin/AppViewModel.kt
@@ -1,5 +1,6 @@
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import org.slf4j.LoggerFactory
 
 class AppViewModel(private val worklogService: WorklogService) : ViewModel() {
     val sprintName = mutableStateOf("")
@@ -8,12 +9,16 @@ class AppViewModel(private val worklogService: WorklogService) : ViewModel() {
     var workDistribution = mutableStateOf("")
     var errorMessage = mutableStateOf("")
     val openErrorDialog = mutableStateOf(false)
+    val log = LoggerFactory.getLogger(this.javaClass)
 
     fun calculateButtonClicked() {
         try {
             workDistribution.value = worklogService.calculateTimeDistribution(
                 sprintName.value, jiraUsername.value, jiraPassword.value).toString()
         } catch (e: Exception) {
+            log.error("Exception while getting Sprint info: "
+                    + e.message + "\n"
+                    + e.stackTraceToString())
             errorMessage.value = e.message ?: "Something went wrong."
             openErrorDialog.value = true
         }
